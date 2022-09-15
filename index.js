cbff0a0ab275cf473f35a852d547f9bf0e6f13e4
amir.bs@openweb.com <amir.bs@openweb.com>
2022-09-15 12:21:03 +0300

    first commit

--- index.js
+++ index.js
@@ -1,42 +1,93 @@
-const express = require('express');
-const morgan = require("morgan");
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require("cors");
 
-// Create Express Server
+const express = require("express");
+const request = require("request");
+const jwt_decode = require("jwt-decode");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
+var bp = require("body-parser");
+// app.use(
+//   bp.urlencoded({
+//     extended: true,
+//   })
+// );
+// app.use(bp.json());
+// // app.use(morgan("dev"));
 
-// Configuration
-const PORT = 3000;
-const HOST = "localhost";
-const API_SERVICE_URL = "https://jsonplaceholder.typicode.com";
+// const corsOptions = {
+//   origin: "http://localhost:8080",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 
-// Logging
-app.use(morgan('dev'));
+// app.use(cors(corsOptions));
+// app.post("/login", async (req, res) => {
+//   verifyUser(req, res);
+// });
 
-// Info GET endpoint
-app.get('/info', (req, res, next) => {
-    res.send('This is a proxy service which proxies to JSONPlaceholder API.');
-});
-
-// Authorization
-app.use('', (req, res, next) => {
-    if (req.headers.authorization) {
-        next();
-    } else {
-        res.sendStatus(403);
-    }
-});
-
-// Proxy endpoints
-app.use('/json_placeholder', createProxyMiddleware({
-    target: API_SERVICE_URL,
-    changeOrigin: true,
-    pathRewrite: {
-        [`^/json_placeholder`]: '',
+const verifyUser = (req, res) => {
+  var options = {
+    method: "POST",
+    url: "http://localhost:8082/realms/amir-tests/protocol/openid-connect/token",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-}));
+    form: {
+      username: req.body.email,
+      password: req.body.password,
+      grant_type: "password",
+      client_id: "myclient",
+      client_secret: "myclient",
+      scope: "openid",
+    },
+  };
 
-// Start Proxy
-app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
-});
+  const temp = {
+    id: "u_od0sO2HUQgMH",
+    user_name: "Inbar27901129",
+    display_name: "Inbar",
+    image_id: "#Green-BottleOpener",
+    email: "inbar.i+2@openweb.com",
+    has_password: true,
+    verified: true,
+    is_moderator: false,
+    is_admin: false,
+    is_journalist: false,
+    is_moderation_viewer: false,
+    is_community_moderator: false,
+    private_profile: false,
+    registered: true,
+    sso_user: false,
+    social: ["twitter"],
+    unseen_notifications_count: null,
+  };
+  request(options, (err, resp) => {
+    const { access_token } = JSON.parse(resp.body);
+    if (!access_token) {
+      res.send({ success: false, type: "2" });
+      return;
+    }
+    const userFromToken = jwt_decode(access_token);
+    const currentUser = temp || jwt_decode(access_token);
+    res.send({
+      currentUser,
+      access_token,
+      refresh_token: resp.body.refresh_token,
+      success: true,
+    });
+  });
+};
+
+app.use(
+  "/",
+  createProxyMiddleware({
+    target: "http://localhost:8082/",
+    changeOrigin: true,
+    onProxyRes,
+  })
+);
+
+function onProxyRes(proxyResponse, request, response) {
+  proxyResponse.headers["amir-token"] = "ssssss";
+}
+app.listen(3321);
